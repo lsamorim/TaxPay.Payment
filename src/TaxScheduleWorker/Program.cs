db2775bc874f1b34f64eeb01a7648f9ab872e30d
Lucas Amorim <lsamorim.it@gmail.com>
2024-02-06 20:24:04 -0300

    Add base project layers

---
+++ src/TaxScheduleWorker/Program.cs
@@ -0,0 +1,10 @@
+using TaxScheduleWorker;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+host.Run();
